841139fa0dfdfe6ea2fbfc9bd47f7d01adc6e4fa
ACExSWAROOP <msgswarupa@gmail.com>
2023-01-13 16:39:55 +0530

    custom titlebar

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from tkinter import ttk
 import sv_ttk as sv
 import os 
 import requests
+from ctypes import windll
 
 def intcheck():
     def internet_stat(url="https://www.google.com/", timeout=3):
@@ -62,35 +63,240 @@ label1 = Label(mainsplash, image=bg_image)
 label1.pack()
 
 def mainwindow():
+        
+    global close_button 
+    global expand_button 
+    global minimize_button
+
     main = Toplevel()
     s= ttk.Style()
     s.configure('.', font=('Segoe UI Variable', 10))
     mainsplash.withdraw()
-    main.state('zoomed')
+    """
     screenwidth = main.winfo_screenwidth()
     screenheight = main.winfo_screenheight()
     app_width = screenwidth
     app_height = screenheight
-    main.geometry(f'{app_width}x{app_height}')
+    """
+    main.geometry("1024x512+75+75")
     main.title("Softhub")
     main.tk.call('wm', 'iconphoto', main._w, ImageTk.PhotoImage(file='images\softhub.ico'))
     main.attributes("-alpha", 0.96)
+    main.overrideredirect(True)
+    main.minimized = False 
+    main.maximized = False    
+    LGRAY = '#3e4042' # button color effects in the title bar (Hex color)
+    RGRAY = '#1c1c1c' # title bar color                       (Hex color)
+    DGRAY = '#1c1c1c' # window background color               (Hex color)
+    title_bar = Frame(main, bg=RGRAY, relief='raised', bd=0,highlightthickness=0)
+    main.attributes("-alpha", 0.95)
+    def set_appwindow(mainWindow): # to display the window icon on the taskbar, 
+                               # even when using main.overrideredirect(True
+    # Some WindowsOS styles, required for task bar integration
+        GWL_EXSTYLE = -20
+        WS_EX_APPWINDOW = 0x00040000
+        WS_EX_TOOLWINDOW = 0x00000080
+        # Magic
+        hwnd = windll.user32.GetParent(mainWindow.winfo_id())
+        stylew = windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
+        stylew = stylew & ~WS_EX_TOOLWINDOW
+        stylew = stylew | WS_EX_APPWINDOW
+        res = windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, stylew)
+    
+        mainWindow.wm_withdraw()
+        mainWindow.after(10, lambda: mainWindow.wm_deiconify())
+        
 
-    main_frame = Frame(main)
-    main_frame.pack(fill=BOTH,expand=1)
+    def minimize_me():
+        main.attributes("-alpha",0) # so you can't see the window when is minimized
+        main.minimized = True       
+
+
+    def deminimize(event):
+
+        main.focus() 
+        main.attributes("-alpha",1) # so you can see the window when is not minimized
+        if main.minimized == True:
+            main.minimized = False                              
+        
+
+    def maximize_me():
+
+        if main.maximized == False: # if the window was not maximized
+            main.normal_size = main.geometry()
+            expand_button.config(text="  🗗  ")
+            main.geometry(f"{main.winfo_screenwidth()}x{main.winfo_screenheight()}+0+0")
+            main.maximized = not main.maximized 
+            # now it's maximized
+        
+        else: # if the window was maximized
+            expand_button.config(text="  ◻  ")
+            main.geometry(main.normal_size)
+            main.maximized = not main.maximized
+            # now it is not maximized
+
+    # put a close button on the title bar
+    
+    close_button = Button(title_bar, text='  ✕  ', command=main.destroy,bg=RGRAY,padx=2,pady=2,font=("calibri", 13),bd=0,fg='white',highlightthickness=0)
+    expand_button = Button(title_bar, text='  ◻  ', command=maximize_me,bg=RGRAY,padx=2,pady=2,bd=0,fg='white',font=("calibri", 13),highlightthickness=0)
+    minimize_button = Button(title_bar, text='  —  ',command=minimize_me,bg=RGRAY,padx=2,pady=2,bd=0,fg='white',font=("calibri", 13),highlightthickness=0)
+    title_bar_title = Label(title_bar, text="Softhub", bg=RGRAY,bd=0,fg='white',font=("helvetica", 10),highlightthickness=0)
+
+    # a frame for the main area of the window, this is where the actual app will go
+    window = Frame(main, bg=DGRAY,highlightthickness=0)
+
+    # pack the widgets
+    title_bar.pack(fill=X)
+    close_button.pack(side=RIGHT,ipadx=7,ipady=1)
+    expand_button.pack(side=RIGHT,ipadx=7,ipady=1)
+    minimize_button.pack(side=RIGHT,ipadx=7,ipady=1)
+    appicon= PhotoImage(file = r"images\softhuḃicon.png")
+    appicon = appicon.subsample(6,6)
+    label = Label(title_bar,image=appicon)
+    label.pack(side=LEFT)
+    title_bar_title.pack(side=LEFT, padx=10)
+    searchicon= PhotoImage(file = r"images\Screenshot 2023-01-12 235851.png")
+    label2 = Label(title_bar,image=searchicon)
+    label2.pack(side=LEFT,padx=235)
+    window.pack(expand=1, fill=BOTH) # replace this with your main Canvas/Frame/etc.
+    #xwin=None
+    #ywin=None
+    # bind title bar motion to the move window function
+
+    def changex_on_hovering(event):
+        global close_button
+        close_button['bg']='red' 
+        
+        
+    def returnx_to_normalstate(event):
+        global close_button
+        close_button['bg']=RGRAY
+        
+
+    def change_size_on_hovering(event):
+        global expand_button
+        expand_button['bg']=LGRAY
+        
+        
+    def return_size_on_hovering(event):
+        global expand_button
+        expand_button['bg']=RGRAY
+        
+
+    def changem_size_on_hovering(event):
+        global minimize_button
+        minimize_button['bg']=LGRAY
+        
+        
+    def returnm_size_on_hovering(event):
+        global minimize_button
+        minimize_button['bg']=RGRAY
+        
+
+    def get_pos(event): # this is executed when the title bar is clicked to move the window
+        if main.maximized == False:
+            xwin = main.winfo_x()
+            ywin = main.winfo_y()
+            startx = event.x_root
+            starty = event.y_root
+
+            ywin = ywin - starty
+            xwin = xwin - startx
+
+            
+            def move_window(event): # runs when window is dragged
+                main.config(cursor="fleur")
+                main.geometry(f'+{event.x_root + xwin}+{event.y_root + ywin}')
+
+
+            def release_window(event): # runs when window is released
+                main.config(cursor="arrow")
+                
+                
+            title_bar.bind('<B1-Motion>', move_window)
+            title_bar.bind('<ButtonRelease-1>', release_window)
+            title_bar_title.bind('<B1-Motion>', move_window)
+            title_bar_title.bind('<ButtonRelease-1>', release_window)
+        else:
+            expand_button.config(text="  ◻  ")
+            main.maximized = not main.maximized
+
+    title_bar.bind('<Button-1>', get_pos) # so you can drag the window from the title bar
+    title_bar_title.bind('<Button-1>', get_pos) # so you can drag the window from the title 
+
+    # button effects in the title bar when hovering over buttons
+    close_button.bind('<Enter>',changex_on_hovering)
+    close_button.bind('<Leave>',returnx_to_normalstate)
+    expand_button.bind('<Enter>', change_size_on_hovering)
+    expand_button.bind('<Leave>', return_size_on_hovering)
+    minimize_button.bind('<Enter>', changem_size_on_hovering)
+    minimize_button.bind('<Leave>', returnm_size_on_hovering)
+
+    # resize the window width
+    resizex_widget = Frame(window,bg=DGRAY,cursor='sb_h_double_arrow')
+    resizex_widget.pack(side=RIGHT,ipadx=2,fill=Y)
+
+
+    def resizex(event):
+        xwin = main.winfo_x()
+        difference = (event.x_root - xwin) - main.winfo_width()
+        
+        if main.winfo_width() > 1024 : # 150 is the minimum width for the window
+            try:
+                main.geometry(f"{ main.winfo_width() + difference }x{ main.winfo_height() }")
+            except:
+                pass
+        else:
+            if difference > 0: # so the window can't be too small (150x150)
+                try:
+                    main.geometry(f"{ main.winfo_width() + difference }x{ main.winfo_height() }")
+                except:
+                    pass
+                
+        resizex_widget.config(bg=DGRAY)
+
+    resizex_widget.bind("<B1-Motion>",resizex)
+
+    # resize the window height
+    resizey_widget = Frame(window,bg=DGRAY,cursor='sb_v_double_arrow')
+    resizey_widget.pack(side=BOTTOM,ipadx=2,fill=X)
+
+    def resizey(event):
+        ywin = main.winfo_y()
+        difference = (event.y_root - ywin) - main.winfo_height()
+
+        if main.winfo_height() > 512: # 150 is the minimum height for the window
+            try:
+                main.geometry(f"{ main.winfo_width()  }x{ main.winfo_height() + difference}")
+            except:
+                pass
+        else:
+            if difference > 0: # so the window can't be too small (150x150)
+                try:
+                    main.geometry(f"{ main.winfo_width()  }x{ main.winfo_height() + difference}")
+                except:
+                    pass
+
+        resizex_widget.config(bg=DGRAY)
+
+    resizey_widget.bind("<B1-Motion>",resizey)
+
+    # some settings
+    main.bind("<FocusIn>",deminimize) # to view the window by clicking on the window icon on the taskbar
+    main.after(10, lambda: set_appwindow(main)) # to see the icon on the task bar
 
     # Create Frame for X Scrollbar
-    sec = Frame(main_frame)
+    sec = Frame(window)
     sec.pack(fill=X,side=BOTTOM)
 
     # Create A Canvas
-    my_canvas = Canvas(main_frame)
+    my_canvas = Canvas(window)
     my_canvas.pack(side=LEFT,fill=BOTH,expand=1)
 
     # Add A Scrollbars to Canvas
     x_scrollbar = ttk.Scrollbar(sec,orient=HORIZONTAL,command=my_canvas.xview)
     x_scrollbar.pack(side=BOTTOM,fill=X)
-    y_scrollbar = ttk.Scrollbar(main_frame,orient=VERTICAL,command=my_canvas.yview)
+    y_scrollbar = ttk.Scrollbar(window,orient=VERTICAL,command=my_canvas.yview)
     y_scrollbar.pack(side=RIGHT,fill=Y)
 
     # Configure the canvas
